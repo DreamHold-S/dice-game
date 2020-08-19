3b7876f3edce92c4a028ead9c2d124e45345fc27
shinebayr <dreamhold01@gmail.com>
2020-08-20 01:26:13 +0800

    duusaw

--- app.js
+++ app.js
@@ -1,36 +1,88 @@
-// o bol negdugeer toglogch 1 bol 2 dugaar toglogch
-var idewhteiToglogch = 0;
-// toglogch onoonuud
-var onoonuud = [0, 0];
-// toglogch onoonud;
-var Onoo = 0;
-// 1-6 hurtelh sanamsargui toonud
-// window.document.querySelector("#score-0").textContent = buusanToo;
-// window.document.querySelector("#score-1").textContent = buusanToo;
-
-window.document.getElementById("score-0").textContent = "0";
-window.document.getElementById("current-0").textContent = "0";
-window.document.getElementById("score-1").textContent = "0";
-window.document.getElementById("current-1").textContent = "0";
-
 var diceDom = document.querySelector(".dice");
+var Onoo;
+var onoonuud;
+var idewhteiToglogch;
+var togloomDuussanEseh;
+function ShineerEhluuleh() {
+  // o bol negdugeer toglogch 1 bol 2 dugaar toglogch
+  idewhteiToglogch = 0;
+  // toglogch onoonuud
+  onoonuud = [0, 0];
+  // toglogch onoonud;
+  Onoo = 0;
+  togloomDuussanEseh = false;
+  // 1-6 hurtelh sanamsargui toonud
+  window.document.getElementById("score-0").textContent = "0";
+  window.document.getElementById("current-0").textContent = "0";
+  window.document.getElementById("score-1").textContent = "0";
+  window.document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
 document
   .querySelector(".btn-roll")
   .addEventListener("click", function shooShideh() {
-    var buusanToo = Math.floor(Math.random() * 6) + 1;
-    diceDom.style.display = "block";
-    diceDom.src = "dice-" + buusanToo + ".png";
+    if (togloomDuussanEseh !== true) {
+      var buusanToo = Math.floor(Math.random() * 6) + 1;
+      diceDom.style.display = "block";
+      diceDom.src = "dice-" + buusanToo + ".png";
 
-    if (buusanToo !== 1) {
-      Onoo = Onoo + buusanToo;
-      document.getElementById("current-" + idewhteiToglogch).textContent = Onoo;
+      if (buusanToo !== 1) {
+        Onoo = Onoo + buusanToo;
+        document.getElementById(
+          "current-" + idewhteiToglogch
+        ).textContent = Onoo;
+      } else {
+        ToglogchEeljSoligdoh();
+      }
     } else {
-      Onoo = 0;
-      document.getElementById("current-" + idewhteiToglogch).textCont;
-      ent = 0;
-      idewhteiToglogch === 0 ? (idewhteiToglogch = 1) : (idewhteiToglogch = 0);
-      document.querySelector(".player-0-panel").classList.toggle("active");
-      document.querySelector(".player-1-panel").classList.toggle("active");
-      diceDom.style.display = "none";
+      alert("Тоглоом шинээр эхлүүлнэ үү");
     }
   });
+document
+  .querySelector(".btn-hold")
+  .addEventListener("click", function shooShideh() {
+    if (togloomDuussanEseh !== true) {
+      onoonuud[idewhteiToglogch] = onoonuud[idewhteiToglogch] + Onoo;
+      document.getElementById("score-" + idewhteiToglogch).textContent =
+        onoonuud[idewhteiToglogch];
+      if (onoonuud[idewhteiToglogch] > 10) {
+        document.getElementById("name-" + idewhteiToglogch).textContent =
+          "Winner !!!";
+        document
+          .querySelector(".player-" + idewhteiToglogch + "-panel")
+          .classList.add("winner");
+        document
+          .querySelector(".player-" + idewhteiToglogch + "-panel")
+          .classList.remove("active");
+        togloomDuussanEseh = true;
+      } else {
+        ToglogchEeljSoligdoh();
+      }
+    } else {
+      alert("Тоглоом шинээр эхлүүлнэ үү");
+    }
+  });
+
+function ToglogchEeljSoligdoh() {
+  Onoo = 0;
+  document.getElementById("current-" + idewhteiToglogch).textCont;
+  ent = 0;
+  idewhteiToglogch === 0 ? (idewhteiToglogch = 1) : (idewhteiToglogch = 0);
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  diceDom.style.display = "none";
+}
+document
+  .querySelector(".btn-new")
+  .addEventListener("click", function InitGame() {
+    ShineerEhluuleh();
+  });
+
+ShineerEhluuleh();
